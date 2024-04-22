fc994b2b3c065b461d1e98ff74afb0a3c3ab7ddf
Osaro Iyoha <saro.iyoha@gmail.com>
2024-04-22 06:50:39 +0100

    update script.js

--- js/script.js
+++ js/script.js
@@ -26,20 +26,36 @@ let yearError = document.querySelector('.error-message-year');
 
 
 
-let isValid ="";
+let isValidDay = false;
+let isValidMonth = false;
+let isValidYear = false;
 
-const setError=(htmlElement, errorText)=>{
+const setError=(time, htmlElement, errorText, status)=>{
   htmlElement.innerHTML = errorText;
-}
+
+  if (time=="day") {
+    time=="day" && status==false ? isValidDay = true : isValidDay = false;
+  } else if(time=="month") {
+    time=="month" && status==false ? isValidMonth = true : isValidMonth = false;
+  } else if(time=="year") {
+    time=="year" && status==false ? isValidYear = true : isValidYear = false;
+  }
+
+  
+  
+  
+  }
 
 const calculateAge = () => {
     const today = new Date();
     const birthDate = new Date(parseInt(inputYear.value), parseInt(inputMonth.value) - 1, parseInt(inputDay.value));
     
     // Validate inputs
-    isNaN(parseInt(inputDay.value)) || parseInt(inputDay.value) <= 0 || parseInt(inputDay.value) > 31 ? setError(dayError, "Must be a valid day") : setError(dayError, "") ;
-    isNaN(parseInt(inputMonth.value)) || parseInt(inputMonth.value) <= 0 || parseInt(inputMonth.value) > 12 ? setError(monthError, "Invalid Month") : setError(monthError, "") ;
-    parseInt(inputYear.value) > today.getFullYear() || isNaN(parseInt(inputYear.value)) ? setError(yearError, "Invalid year") : setError(yearError, "");
+    isNaN(parseInt(inputDay.value)) || parseInt(inputDay.value) <= 0 || parseInt(inputDay.value) > 31 ? setError("day", dayError, "Must be a valid day", true) : setError("day", dayError, "", false ) ;
+
+    isNaN(parseInt(inputMonth.value)) || parseInt(inputMonth.value) <= 0 || parseInt(inputMonth.value) > 12 ? setError("month",monthError, "Invalid Month", true) : setError("month", monthError, "", false) ;
+    
+    parseInt(inputYear.value) > today.getFullYear() || isNaN(parseInt(inputYear.value)) ? setError("year", yearError, "Invalid year", true) : setError("year", yearError, "", false);
 
     
 
@@ -62,9 +78,17 @@ const calculateAge = () => {
 
     console.log({ years, months, days })
 
-    resultYear.innerHTML = years;
-    resultMonth.innerHTML = months;
-    resultDay.innerHTML = days;
+    if (!isValidDay || !isValidMonth || !isValidYear) {
+      resultYear.innerHTML = '- -';
+      resultMonth.innerHTML = '- -';
+      resultDay.innerHTML = '- -';
+    } else {
+      resultYear.innerHTML = years;
+      resultMonth.innerHTML = months;
+      resultDay.innerHTML = days;
+    }
+    
+    console.log(isValidDay, isValidMonth, isValidYear)
   };
 
 
